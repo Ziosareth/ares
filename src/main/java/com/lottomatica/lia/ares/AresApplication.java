4482f465f39f1dc4096414afe548b091628b3385
Alessio Bivi <ale.bivi94@gmail.com>
2023-06-09 17:21:57 +0200

    removed unnecessary infrastructure code

--- src/main/java/com/lottomatica/lia/ares/AresApplication.java
+++ src/main/java/com/lottomatica/lia/ares/AresApplication.java
@@ -1,18 +1,10 @@
 package com.lottomatica.lia.ares;
 
 import com.lottomatica.lia.ares.properties.WorkflowConfiguration;
-import io.micrometer.common.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.core.env.Environment;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 @Slf4j
 @SpringBootApplication
@@ -20,52 +12,8 @@ import java.util.Optional;
 public class AresApplication {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(AresApplication.class);
-        addDefaultProfile(app);
-        Environment env = app.run(args).getEnvironment();
-        logApplicationStartup(env);
-    }
-
-    //To be moved in a common lib
-    public static void addDefaultProfile(SpringApplication app) {
-        Map<String, Object> defProperties = new HashMap<>();
-        defProperties.put("spring.profiles.default", "local");
-        defProperties.put("spring.config.name", "ares");
-        app.setDefaultProperties(defProperties);
+        SpringApplication.run(AresApplication.class, args);
     }
 
 
-    private static void logApplicationStartup(Environment env) {
-        String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
-        String serverPort = env.getProperty("server.port");
-        String contextPath = Optional
-                .ofNullable(env.getProperty("spring.webflux.base-path"))
-                .filter(StringUtils::isNotBlank)
-                .orElse("/");
-        String hostAddress = "localhost";
-        try {
-            hostAddress = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            log.warn("The host name could not be determined, using `localhost` as fallback");
-        }
-        log.info(
-                """
-
-                        ----------------------------------------------------------
-                        \tApplication '{}' is running! Access URLs:
-                        \tLocal: \t\t{}://localhost:{}{}
-                        \tExternal: \t{}://{}:{}{}
-                        \tProfile(s): \t{}
-                        ----------------------------------------------------------""",
-                env.getProperty("spring.application.name"),
-                protocol,
-                serverPort,
-                contextPath,
-                protocol,
-                hostAddress,
-                serverPort,
-                contextPath,
-                env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles()
-        );
-    }
 }
