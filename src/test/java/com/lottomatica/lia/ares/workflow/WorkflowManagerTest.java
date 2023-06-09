ba10a3950c2162f370c2ef6417ceefe4cdc88106
Alessio Bivi <ale.bivi94@gmail.com>
2023-06-09 17:17:49 +0200

    cle

--- src/test/java/com/lottomatica/lia/ares/workflow/WorkflowManagerTest.java
+++ src/test/java/com/lottomatica/lia/ares/workflow/WorkflowManagerTest.java
@@ -8,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -49,14 +48,5 @@ class WorkflowManagerTest {
                 .expectNext(stepResponse.body())
                 .expectComplete()
                 .verify();
-    }
-
-
-    @Test
-    void lol(){
-        Flux<Integer> flux = Flux.range(0, 100).doOnNext(System.out::println);
-        Mono<String> lol = Mono.just("abc").doOnNext(System.out::println);
-        Mono<Void> then = Mono.when(lol,flux).then();
-        StepVerifier.create(then).expectSubscription().verifyComplete();
     }
 }
